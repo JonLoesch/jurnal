bb24addc169744d445d6294ad715b736e900ef07
Jonathan Loesch <jonloesch@gmail.com>
2023-12-22 16:42:24 -0500

    Indeterminate checkboxes

--- src/components/metrics/checkbox.tsx
+++ src/components/metrics/checkbox.tsx
@@ -1,20 +1,28 @@
 import { Metric } from "~/lib/metricSchemas";
 import { MetricUI } from ".";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const checkbox: MetricUI<"checkbox"> = {
   Edit(props) {
     const v = useRef(props.value?.value);
+    const [state, setState] = useState(props.value?.value)
+    const ref = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+      if (ref.current) {
+        ref.current.indeterminate = (state === undefined);
+      }
+    }, [state]);
     return (
       <input
+        ref={ref}
         id={`metric-${props.metricId}`}
         type="checkbox"
-        className="checkbox"
-        checked={v.current}
+        className="checkbox indeterminate:checkbox-warning"
+        checked={state}
         onChange={(e) => {
           const checked = e.target.checked;
           props.onChange(checked);
-          v.current = checked;
+          setState(checked);
         }}
       />
     );
